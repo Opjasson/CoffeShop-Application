ea58d64f03347f66058fbc51a407f600aa535701
Opjasson <azmitheusos333@gmail.com>
2025-12-15 17:28:30 +0700

    mainActivity : done show banner to home page from DB firebase

---
+++ app/src/main/java/com/example/coffeshop/ViewModel/MainViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.coffeshop.ViewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.example.coffeshop.Domain.BannerModel
+import com.example.coffeshop.Domain.CategoryModel
+import com.example.coffeshop.Repository.MainRepository
+
+class MainViewModel: ViewModel() {
+    private val repository= MainRepository()
+
+    fun loadBanner(): LiveData<MutableList<BannerModel>>{
+        return repository.loadBanner()
+    }
+
+    fun loadCategory(): LiveData<MutableList<CategoryModel>>{
+        return repository.loadCategory()
+    }
+}
