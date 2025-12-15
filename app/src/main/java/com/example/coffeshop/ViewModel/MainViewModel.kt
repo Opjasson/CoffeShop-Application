e747047009e13a0cd476a5852f0e843474b5b54a
Opjasson <azmitheusos333@gmail.com>
2025-12-15 20:56:53 +0700

    layout : create viewholder_populer

--- app/src/main/java/com/example/coffeshop/ViewModel/MainViewModel.kt
+++ app/src/main/java/com/example/coffeshop/ViewModel/MainViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.example.coffeshop.Domain.BannerModel
 import com.example.coffeshop.Domain.CategoryModel
+import com.example.coffeshop.Domain.ItemsModel
 import com.example.coffeshop.Repository.MainRepository
 
 class MainViewModel: ViewModel() {
@@ -15,5 +16,9 @@ class MainViewModel: ViewModel() {
 
     fun loadCategory(): LiveData<MutableList<CategoryModel>>{
         return repository.loadCategory()
+    }
+
+    fun loadPopular(): LiveData<MutableList<ItemsModel>> {
+        return repository.loadPopular()
     }
 }
