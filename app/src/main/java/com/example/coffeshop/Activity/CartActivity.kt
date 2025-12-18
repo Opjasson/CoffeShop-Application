4ddad593eadb41812efe2e6e7a05f0f7d3e059eb
Opjasson <azmitheusos333@gmail.com>
2025-12-18 18:35:19 +0700

    CartAdapter : done setting adapter cart

--- app/src/main/java/com/example/coffeshop/Activity/CartActivity.kt
+++ app/src/main/java/com/example/coffeshop/Activity/CartActivity.kt
@@ -5,17 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.coffeshop.Helper.ManagmentCart
 import com.example.coffeshop.R
+import com.example.coffeshop.databinding.ActivityCartBinding
 
 class CartActivity : AppCompatActivity() {
+    lateinit var binding: ActivityCartBinding
+    lateinit var managmentCart: ManagmentCart
+    private var tax: Double=0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_cart)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
